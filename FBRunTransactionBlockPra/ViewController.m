52e7bcfae0a65c09e172027d385f40a9c78ed079
Ling <asklingtoby@gmail.com>
2016-08-15 01:17:31 -0400

    add runTransactionBlock function

--- FBRunTransactionBlockPra/ViewController.m
+++ FBRunTransactionBlockPra/ViewController.m
@@ -7,21 +7,96 @@
 //
 
 #import "ViewController.h"
+@import FirebaseAuth;
+@import Firebase;
+@import FirebaseDatabase;
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+FIRDatabaseReference *ref;
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self loginUserToFirebase];
+    ref = [[FIRDatabase database]reference];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)loginUserToFirebase{
+    NSString *newPwd = @"111111";
+    [[FIRAuth auth] signInWithEmail:@"ling@gmail.com"
+                           password:newPwd
+                         completion:^(FIRUser *user, NSError *error) {
+                             
+                             if (error) {
+                                 NSString *message=@"Invalid email or password";
+                                 NSString *alertTitle=@"Invalid!";
+                                 NSString *OKText=@"OK";
+                                 
+                                 UIAlertController *alertView = [UIAlertController alertControllerWithTitle:alertTitle message:message preferredStyle:UIAlertControllerStyleAlert];
+                                 UIAlertAction *alertAction = [UIAlertAction actionWithTitle:OKText style:UIAlertActionStyleCancel handler:nil];
+                                 [alertView addAction:alertAction];
+                                 [self presentViewController:alertView animated:YES completion:nil];
+                             }else{
+                                 NSLog(@"Login success!!!!!!!!!!!!");
+                                 
+                             }
+                         }];
 }
+- (IBAction)signOutBtnPressed:(id)sender {
+    NSError *error;
+    [[FIRAuth auth] signOut:&error];
+    if (!error) {
+        
+    }
+}
+
+- (IBAction)likePost:(id)sender {
+    [self likeThePost];
+}
+
+-(void)likeThePost{
+    [ref runTransactionBlock:^FIRTransactionResult * _Nonnull(FIRMutableData * _Nonnull currentData) {
+        NSMutableDictionary *post = currentData.value;
+        if (!post || [post isEqual:[NSNull null]]) {
+            return [FIRTransactionResult successWithValue:currentData];
+        }
+        
+        NSMutableDictionary *stars = [post objectForKey:@"stars"];
+        if (!stars) {
+            stars = [[NSMutableDictionary alloc] initWithCapacity:1];
+        }
+        NSString *uid = [FIRAuth auth].currentUser.uid;
+        int starCount = [post[@"starCount"] intValue];
+        if ([stars objectForKey:uid]) {
+            // Unstar the post and remove self from stars
+            starCount--;
+            [stars removeObjectForKey:uid];
+        } else {
+            // Star the post and add self to stars
+            starCount++;
+            stars[uid] = @YES;
+        }
+        post[@"stars"] = stars;
+        post[@"starCount"] = [NSNumber numberWithInt:starCount];
+        
+        // Set value and report transaction success
+        [currentData setValue:post];
+        return [FIRTransactionResult successWithValue:currentData];
+    } andCompletionBlock:^(NSError * _Nullable error,
+                           BOOL committed,
+                           FIRDataSnapshot * _Nullable snapshot) {
+        // Transaction completed
+        if (error) {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
+
+}
+
+
 
 @end
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FBRunTransactionBlockPra' do
+  # Uncomment this line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+  
+  pod 'Firebase'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
+  pod 'Firebase/Core'
+
+  # Pods for FBRunTransactionBlockPra
+
+end
