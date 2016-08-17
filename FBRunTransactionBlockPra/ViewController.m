2dc5b2f6f500015715472f665d96d8c6ee0de489
Ling <asklingtoby@gmail.com>
2016-08-16 23:42:59 -0400

    simplify the demo

--- FBRunTransactionBlockPra/ViewController.m
+++ FBRunTransactionBlockPra/ViewController.m
@@ -21,17 +21,16 @@ FIRDatabaseReference *ref;
 
 
 - (void)viewDidLoad {
+    [self loginUserToFirebase];
     [super viewDidLoad];
     ref = [[FIRDatabase database]reference];
 }
-- (IBAction)login:(id)sender {
-    [self loginUserToFirebase];
-}
+
 
 
 -(void)loginUserToFirebase{
     NSString *newPwd = @"111111";
-    [[FIRAuth auth] signInWithEmail:_emailTextField.text
+    [[FIRAuth auth] signInWithEmail:@"ling@gmail.com"
                            password:newPwd
                          completion:^(FIRUser *user, NSError *error) {
                              
@@ -56,9 +55,6 @@ FIRDatabaseReference *ref;
     [self addCount];
 }
 
-- (IBAction)likePost:(id)sender {
-    [self likeThePost];
-}
 
 
 -(void)addCount{
@@ -69,56 +65,17 @@ FIRDatabaseReference *ref;
         if (count == [NSNull null]) {
             count = 0;
         }else{
-            currentData.value = @([count integerValue] +1);
-        }
-        
-        return [FIRTransactionResult successWithValue:currentData];
-        
-    }];
-}
-
--(void)likeThePost{
-    [ref runTransactionBlock:^FIRTransactionResult * _Nonnull(FIRMutableData * _Nonnull currentData) {
-        NSMutableDictionary *post = currentData.value;
-        if (!post || [post isEqual:[NSNull null]]) {
-            return [FIRTransactionResult successWithValue:currentData];
-        }
-        
-        NSMutableDictionary *stars = [post objectForKey:@"stars"];
-        if (!stars) {
-            stars = [[NSMutableDictionary alloc] initWithCapacity:1];
-        }
-        NSString *uid = [FIRAuth auth].currentUser.uid;
-        int starCount = [post[@"starCount"] intValue];
-        if ([stars objectForKey:uid]) {
-            // Unstar the post and remove self from stars
-            starCount--;
-            [stars removeObjectForKey:uid];
-        } else {
-            // Star the post and add self to stars
-            
-            for (int i=0; i<100000; i++) {
-                starCount++;
-                stars[uid] = @YES;
-                NSLog(@"the current starCount is %d",starCount);
+            for (int i = 0; i < 50001; i++) {
+                currentData.value = @([count integerValue] +i);
+                NSLog(@"%@",currentData.value);
             }
         }
-        post[@"stars"] = stars;
-        post[@"starCount"] = [NSNumber numberWithInt:starCount];
-        
-        // Set value and report transaction success
-        [currentData setValue:post];
         return [FIRTransactionResult successWithValue:currentData];
-    } andCompletionBlock:^(NSError * _Nullable error,
-                           BOOL committed,
-                           FIRDataSnapshot * _Nullable snapshot) {
-        // Transaction completed
-        if (error) {
-            NSLog(@"%@", error.localizedDescription);
-        }
+        
     }];
-
 }
+
+
 
 
 
